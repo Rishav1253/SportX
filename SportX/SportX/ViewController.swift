11530805b6b77e8308bb2b6a4d8cb4867c93e31f
Rishav1253 <drishav1253@gmail.com>
2024-05-08 12:27:49 +0530

    first commit

---
+++ SportX/SportX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportX
+//
+//  Created by Rishav Dey on 08/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
